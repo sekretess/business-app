80e883c38fb63ddfde25cc4050c083fc363ad7de
Nazir <nazir.alicanov@gmail.com>
2024-05-14 22:18:44 +0200

    fix: Initialize app

---
+++ src/main/java/com/sekretess/SekretessBusinessAppApplication.java
@@ -0,0 +1,13 @@
+package com.sekretess;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SekretessBusinessAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SekretessBusinessAppApplication.class, args);
+	}
+
+}
