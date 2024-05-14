7121200a8dccd12756e8b95c9c0f73ca4e6e04b9
Nazir <nazir.alicanov@gmail.com>
2024-05-14 22:18:44 +0200

    Initialize app

---
+++ src/test/java/com/sekretess/SekretessBusinessAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sekretess;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SekretessBusinessAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
